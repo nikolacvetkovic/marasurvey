43c336708197969070f84859a5b855e52227ee93
Nikola <nikola.cvetkovic91@gmail.com>
2017-06-26 08:01:21 +0000

    change database-url

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var bodyParser  = require("body-parser"),
+var express     = require("express"),
+    bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    express     = require("express"),
     server      = express();
 
-mongoose.connect("mongodb://localhost/marasurveydevelop");
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
+
+var surveySchema = new mongoose.Schema({
+    gender: String,
+});
+
+var Survey = mongoose.model("Survey", surveySchema);
 
 server.set("view engine", "ejs");
 server.use(express.static("public"));
@@ -11,6 +17,11 @@ server.use(bodyParser.urlencoded({extended: true}));
 
 server.get("/", function(req, res){
    res.render("index"); 
+});
+
+server.post("/survey", function(req, res){
+    console.log(req.body.gender);
+    res.redirect("/");
 });
 
 server.listen(process.env.PORT, process.env.IP, function() {
